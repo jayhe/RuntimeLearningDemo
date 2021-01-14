260637ea20449fd47e606750820304231393687b
jayhe <1054134542@qq.com>
2021-01-14 17:34:48 +0800

    Dobby使用真机包，支持arm64 armv7

--- HCInject/Dobby/Dobby.framework/Headers/dobby.h
+++ HCInject/Dobby/Dobby.framework/Headers/dobby.h
@@ -1,6 +1,13 @@
 #ifndef dobby_h
 #define dobby_h
 
+// obfuscated interface
+#if 0
+#define DobbyBuildVersion c343f74888dffad84d9ad08d9c433456
+#define DobbyHook         c8dc3ffa44f22dbd10ccae213dd8b1f8
+#define DobbyInstrument   b71e27bca2c362de90c1034f19d839f9
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -8,11 +15,26 @@ extern "C" {
 #include <stdbool.h>
 #include <stdint.h>
 
+typedef enum {
+  kMemoryOperationSuccess,
+  kMemoryOperationError,
+  kNotSupportAllocateExecutableMemory,
+  kNotEnough,
+  kNone
+} MemoryOperationError;
+
+#define PLATFORM_INTERFACE_CODE_PATCH_TOOL_H
+MemoryOperationError CodePatch(void *address, uint8_t *buffer, uint32_t buffer_size);
+
+typedef uintptr_t addr_t;
+typedef uint32_t  addr32_t;
+typedef uint64_t  addr64_t;
+
 #if defined(__arm64__) || defined(__aarch64__)
-#define Tx(type) type##arm64
-#define TX() type##ARM64
-#define xT() arm64##type
-#define XT() ARM64##type
+
+#define ARM64_TMP_REG_NDX_0 17
+
+// float register
 typedef union _FPReg {
   __int128_t q;
   struct {
@@ -27,9 +49,12 @@ typedef union _FPReg {
   } f;
 } FPReg;
 
+// register context
 typedef struct _RegisterContext {
-  uint64_t dmmpy_0;
+  uint64_t dmmpy_0; // dummy placeholder
+  uint64_t sp;
 
+  uint64_t dmmpy_1; // dummy placeholder
   union {
     uint64_t x[29];
     struct {
@@ -42,20 +67,23 @@ typedef struct _RegisterContext {
   uint64_t lr;
 
   union {
-    FPReg q[8];
+    FPReg q[32];
     struct {
       FPReg q0, q1, q2, q3, q4, q5, q6, q7;
+      // [!!! READ ME !!!]
+      // for Arm64, can't access q8 - q31, unless you enable full floating-point register pack
+      FPReg q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20, q21, q22, q23, q24, q25, q26, q27, q28, q29,
+          q30, q31;
     } regs;
   } floating;
 } RegisterContext;
 #elif defined(__arm__)
-#define Tx(type) type##arm
-#define TX() type##ARM
-#define xT() arm##type
-#define XT() ARM##type
 typedef struct _RegisterContext {
   uint32_t dummy_0;
   uint32_t dummy_1;
+
+  uint32_t dummy_2;
+  uint32_t sp;
 
   union {
     uint32_t r[13];
@@ -66,18 +94,38 @@ typedef struct _RegisterContext {
 
   uint32_t lr;
 } RegisterContext;
-#elif defined(_M_X64) || defined(__x86_64__)
+#elif defined(_M_IX86) || defined(__i386__)
 typedef struct _RegisterContext {
-  uint64_t flags;
+  uint32_t dummy_0;
+  uint32_t esp;
+
+  uint32_t dummy_1;
+  uint32_t flags;
+
   union {
     struct {
-      uint64_t r15, r14, r13, r12, r11, r10, r9, r8, rsi, rdi, rsp, rbp, rdx, rcx, rbx, rax;
+      uint32_t eax, ebx, ecx, edx, ebp, esp, edi, esi;
     } regs;
   } general;
+
+} RegisterContext;
+#elif defined(_M_X64) || defined(__x86_64__)
+typedef struct _RegisterContext {
+  uint64_t dummy_0;
+  uint64_t rsp;
+
+  union {
+    struct {
+      uint64_t rax, rbx, rcx, rdx, rbp, rsp, rdi, rsi, r8, r9, r10, r11, r12, r13, r14, r15;
+    } regs;
+  } general;
+
+  uint64_t dummy_1;
+  uint64_t flags;
 } RegisterContext;
 #endif
 
-#define RT_FAILED -1
+#define RT_FAILED  -1
 #define RT_SUCCESS 0
 typedef enum _RetStatus { RS_FAILED = -1, RS_SUCCESS = 0 } RetStatus;
 
@@ -97,6 +145,7 @@ typedef struct _HookEntryInfo {
   };
 } HookEntryInfo;
 
+// DobbyWrap <==> DobbyInstrument, so use DobbyInstrument instead of DobbyWrap
 #if 0
 // wrap function with pre_call and post_call
 typedef void (*PreCallTy)(RegisterContext *reg_ctx, const HookEntryInfo *info);
@@ -104,20 +153,38 @@ typedef void (*PostCallTy)(RegisterContext *reg_ctx, const HookEntryInfo *info);
 int DobbyWrap(void *function_address, PreCallTy pre_call, PostCallTy post_call);
 #endif
 
+// return dobby build date
+const char *DobbyBuildVersion();
+
 // replace function
 int DobbyHook(void *function_address, void *replace_call, void **origin_call);
 
 // dynamic binary instrument for instruction
+// [!!! READ ME !!!]
+// for Arm64, can't access q8 - q31, unless you enable full floating-point register pack
 typedef void (*DBICallTy)(RegisterContext *reg_ctx, const HookEntryInfo *info);
-int DobbyInstrument(void *inst_address, DBICallTy dbi_call);
+int DobbyInstrument(void *instr_address, DBICallTy dbi_call);
+
+// destory and restore hook
+int DobbyDestroy(void *address);
 
 // iterate symbol table and find symbol
-void *DobbyFindSymbol(const char *image_name, const char *symbol_name);
+void *DobbySymbolResolver(const char *image_name, const char *symbol_name);
 
-// bxx branch plugin
-#if defined(__arm64__) || defined(__aarch64__) || defined(__arm__)
-void dobby_enable_arm64_bxx_branch_trampoline();
+// global offset table
+int DobbyGlobalOffsetTableReplace(char *image_name, char *symbol_name, void *fake_func, void **orig_func);
+
+// [!!! READ ME !!!]
+// for arm, Arm64, dobby will use b xxx instead of ldr absolute indirect branch
+// for x64, dobby always use absolute indirect jump
+#if defined(__arm__) || defined(__arm64__) || defined(__aarch64__) || defined(_M_X64) || defined(__x86_64__)
+void dobby_enable_near_branch_trampoline();
+void dobby_disable_near_branch_trampoline();
 #endif
+
+// register linker load image callback
+typedef void (*linker_load_callback_t)(const char *image_name, void *handle);
+void dobby_register_image_load_callback(linker_load_callback_t func);
 
 #ifdef __cplusplus
 }
